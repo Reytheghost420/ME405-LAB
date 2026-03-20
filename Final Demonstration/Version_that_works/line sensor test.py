5d724960f5943a54138768851742920185246b0a
Reytheghost420 <reyfarias6@gmail.com>
2026-03-20 04:02:47 -0700

    Add files via upload

---
+++ Final Demonstration/Version_that_works/line sensor test.py
@@ -0,0 +1,23 @@
+from machine import Pin, ADC
+import time
+
+# Create ADC objects for each sensor output pin
+adc_pins = [
+    ADC(Pin("PA4")),
+    ADC(Pin("PB0")),
+    ADC(Pin("PC1")),
+    ADC(Pin("PC0")),
+    ADC(Pin("PC2")),
+    ADC(Pin("PC3")),
+    ADC(Pin("PC5")),
+]
+
+print("QTR 7-Channel Analog Sensor Test")
+print("Move sensor over white and black surfaces...\n")
+
+while True:
+    values = [adc.read_u16() for adc in adc_pins]
+
+    print(values)
+
+    time.sleep_ms(200)
